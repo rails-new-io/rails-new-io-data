9fd6c58916b7ed5d4da68c7da2bada9fee1b3f20
railsnew.io <trinity@railsnew.io>
2025-05-03 08:59:53 +0200

    Update ingredient: RSpec

--- ingredients/RSpec/template.rb
+++ ingredients/RSpec/template.rb
@@ -1,36 +1,9 @@
-puts "DEBUG: Starting template execution"
-puts "DEBUG: Current directory: #{Dir.pwd}"
-puts "DEBUG: Environment variables:"
-puts "DEBUG: BUNDLE_APP_CONFIG: #{ENV['BUNDLE_APP_CONFIG']}"
-puts "DEBUG: BUNDLE_CONFIG: #{ENV['BUNDLE_CONFIG']}"
-puts "DEBUG: BUNDLE_WITHOUT: #{ENV['BUNDLE_WITHOUT']}"
-puts "DEBUG: BUNDLE_IGNORE_CONFIG: #{ENV['BUNDLE_IGNORE_CONFIG']}"
-puts "DEBUG: BUNDLE_USER_HOME: #{ENV['BUNDLE_USER_HOME']}"
-puts "DEBUG: GEM_HOME: #{ENV['GEM_HOME']}"
-puts "DEBUG: GEM_PATH: #{ENV['GEM_PATH']}"
-puts "DEBUG: PATH: #{ENV['PATH']}"
-
-puts "DEBUG: Bundler config before any changes:"
-puts `bundle config`
-
-puts "DEBUG: Adding rspec-rails to development/test group"
 gem_group :development, :test do
   gem "rspec-rails"
 end
 
 run 'bundle install'
 
-puts "DEBUG: PATH after bundle install:"
-puts ENV['PATH']
-puts "DEBUG: which bundle after bundle install:"
-puts `which bundle`
-puts "DEBUG: Bundler config after bundle install:"
-puts `bundle config`
-
-puts "DEBUG: Generating rspec:install"
 generate "rspec:install"
 
-puts "DEBUG: Removing test directory"
 run "rm -rf test"
-
-puts "DEBUG: Template execution completed"
